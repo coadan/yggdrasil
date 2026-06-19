bd73c17e88012629c13748bd6483ff4a04a29e38
Vegard Steen <vegard@breyta.io>
2026-06-19 15:34:22 +0200

    Split miscellaneous package facts

--- src/agraph/extract/package.clj
+++ src/agraph/extract/package.clj
@@ -9,26 +9,9 @@
             [agraph.extract.package-cargo-go :as package-cargo-go]
             [agraph.extract.package-mobile :as package-mobile]
             [agraph.extract.package-jvm-dotnet :as package-jvm-dotnet]
+            [agraph.extract.package-misc :as package-misc]
             [clojure.string :as str]))
 
-(defn- ruby-gem-dependencies
-  [content]
-  (->> (str/split-lines content)
-       (map-indexed vector)
-       (keep (fn [[idx line]]
-               (when-let [[_ gem-name]
-                          (re-matches
-                           #"^\s*(?:gem|spec\.add_(?:runtime_)?dependency|spec\.add_development_dependency)\s+['\"]([^'\"]+)['\"].*"
-                           line)]
-                 (common/package-fact {:ecosystem :rubygems
-                                :package-name gem-name
-                                :source-line (inc idx)}))))
-       distinct
-       vec))
-(defn- gemspec-name
-  [content]
-  (some-> (re-find #"(?m)^\s*[^#\n]*\.name\s*=\s*['\"]([^'\"]+)['\"]" content)
-          second))
 (defn- yaml-section-keys
   [content section-names]
   (let [sections (set section-names)]
@@ -236,66 +219,6 @@
                 :label label
                 :source-line source-line
                 :relation :references}))))
-(defn- composer-project-name
-  [content path]
-  (or (:name (common/read-json-map content))
-      path))
-(defn- composer-dependency-scope
-  [k]
-  (case k
-    :require "runtime"
-    :require-dev "development"
-    nil))
-(defn- composer-json-facts
-  [content]
-  (if-let [m (common/read-json-map content)]
-    (->> [:require :require-dev]
-         (mapcat
-          (fn [k]
-            (when-let [deps (get m k)]
-              (when (map? deps)
-                (map (fn [[package-name version]]
-                       (common/package-fact {:ecosystem :composer
-                                      :package-name (common/json-key-label package-name)
-                                      :version-range (when (string? version)
-                                                       version)
-                                      :dependency-scope (composer-dependency-scope k)
-                                      :source-line 1}))
-                     deps)))))
-         (remove nil?)
-         distinct
-         vec)
-    []))
-(defn- deps-edn-project-name
-  [path]
-  path)
-(defn- deps-edn-dependencies
-  [content]
-  (try
-    (let [parsed (edn/read-string content)
-          dep-entries (concat (:deps parsed)
-                              (mapcat :extra-deps (vals (:aliases parsed))))]
-      (->> dep-entries
-           (keep (fn [[dep-name spec]]
-                   (cond
-                     (:mvn/version spec)
-                     (common/package-fact {:ecosystem :maven
-                                    :package-name (str dep-name)
-                                    :version-range (:mvn/version spec)
-                                    :source-line 1})
-
-                     (:git/url spec)
-                     (common/package-fact {:ecosystem :git
-                                    :package-name (:git/url spec)
-                                    :version-range (or (:git/sha spec)
-                                                       (:git/tag spec))
-                                    :source-line 1})
-
-                     :else nil)))
-           distinct
-           vec))
-    (catch Exception _
-      [])))
 (defn- manifest-facts
   [{:keys [path content]}]
   (let [filename (common/manifest-name path)
@@ -349,8 +272,8 @@
        :facts (package-cargo-go/go-work-uses content)}
 
       (= "composer.json" filename)
-      {:project-label (composer-project-name content path)
-       :facts (composer-json-facts content)}
+      {:project-label (package-misc/composer-project-name content path)
+       :facts (package-misc/composer-json-facts content)}
 
       (= "pyproject.toml" filename)
       {:project-label (package-python/pyproject-name content path)
@@ -369,13 +292,13 @@
        :facts (package-python/pipfile-dependencies content)}
 
       (= "deps.edn" filename)
-      {:project-label (deps-edn-project-name path)
-       :facts (deps-edn-dependencies content)}
+      {:project-label (package-misc/deps-edn-project-name path)
+       :facts (package-misc/deps-edn-dependencies content)}
 
       (or (= "gemfile" filename)
           (= "gemspec" extension))
-      {:project-label (or (gemspec-name content) path)
-       :facts (ruby-gem-dependencies content)}
+      {:project-label (or (package-misc/gemspec-name content) path)
+       :facts (package-misc/ruby-gem-dependencies content)}
 
       (= "pubspec.yaml" filename)
       {:project-label (or (common/yaml-top-level-value content "name") path)
---
+++ src/agraph/extract/package_misc.clj
@@ -0,0 +1,83 @@
+(ns agraph.extract.package-misc
+  (:require [agraph.extract.common :as common]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+(defn ruby-gem-dependencies
+  [content]
+  (->> (str/split-lines content)
+       (map-indexed vector)
+       (keep (fn [[idx line]]
+               (when-let [[_ gem-name]
+                          (re-matches
+                           #"^\s*(?:gem|spec\.add_(?:runtime_)?dependency|spec\.add_development_dependency)\s+['\"]([^'\"]+)['\"].*"
+                           line)]
+                 (common/package-fact {:ecosystem :rubygems
+                                :package-name gem-name
+                                :source-line (inc idx)}))))
+       distinct
+       vec))
+(defn gemspec-name
+  [content]
+  (some-> (re-find #"(?m)^\s*[^#\n]*\.name\s*=\s*['\"]([^'\"]+)['\"]" content)
+          second))
+(defn composer-project-name
+  [content path]
+  (or (:name (common/read-json-map content))
+      path))
+(defn- composer-dependency-scope
+  [k]
+  (case k
+    :require "runtime"
+    :require-dev "development"
+    nil))
+(defn composer-json-facts
+  [content]
+  (if-let [m (common/read-json-map content)]
+    (->> [:require :require-dev]
+         (mapcat
+          (fn [k]
+            (when-let [deps (get m k)]
+              (when (map? deps)
+                (map (fn [[package-name version]]
+                       (common/package-fact {:ecosystem :composer
+                                      :package-name (common/json-key-label package-name)
+                                      :version-range (when (string? version)
+                                                       version)
+                                      :dependency-scope (composer-dependency-scope k)
+                                      :source-line 1}))
+                     deps)))))
+         (remove nil?)
+         distinct
+         vec)
+    []))
+(defn deps-edn-project-name
+  [path]
+  path)
+(defn deps-edn-dependencies
+  [content]
+  (try
+    (let [parsed (edn/read-string content)
+          dep-entries (concat (:deps parsed)
+                              (mapcat :extra-deps (vals (:aliases parsed))))]
+      (->> dep-entries
+           (keep (fn [[dep-name spec]]
+                   (cond
+                     (:mvn/version spec)
+                     (common/package-fact {:ecosystem :maven
+                                    :package-name (str dep-name)
+                                    :version-range (:mvn/version spec)
+                                    :source-line 1})
+
+                     (:git/url spec)
+                     (common/package-fact {:ecosystem :git
+                                    :package-name (:git/url spec)
+                                    :version-range (or (:git/sha spec)
+                                                       (:git/tag spec))
+                                    :source-line 1})
+
+                     :else nil)))
+           distinct
+           vec))
+    (catch Exception _
+      [])))
